c78bcb12bcb61aa9bb49d1ad3490392d49bce400
JosephChawan <71522575+JosephChawan@users.noreply.github.com>
2020-09-26 13:36:17 -0700

    Add files via upload

---
+++ library.cpp
@@ -0,0 +1,23 @@
+#include "library.h"
+
+//Use this file to include the implementations of your functions.
+
+void getOneStudent(ifstream& input, Student& s) {
+
+} //end of getOneStudent
+
+void displayAllStudents(const Student arr[], int n) {
+  
+} //end of displayAllStudents
+
+int findStudent(const Student arr[], int n, const string& id) {
+  return -1;
+} //end of findStudent
+
+void updateStudent(Student& s, int units, char letter) {
+
+} //end of updateStudent
+
+void sortByStartSession(Student arr[], int n) {
+  
+} //end of sortByStartSession
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "library.h"
+using namespace std;
+
+
+
+int main() {
+  //use this area to include the main flow of your program and make calls to the functions.
+  cout << "Updating student records ..." << endl;
+  return 0;
+} 
